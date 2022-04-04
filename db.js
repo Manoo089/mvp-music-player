a780296a6017a82970fe69452f6de0d65959941b
Manuel Hohenadl <manoo089@googlemail.com>
2022-04-04 14:36:54 +0200

    fixed: link to music

--- db.js
+++ db.js
@@ -4,12 +4,12 @@ const songs = [{
         title: "Waterdrop",
         artist: "Manoo & Shirin",
         cover: "https://cdn.musicvine.com/images/trinity-avatar-v1_8164557820960638.jpg",
-        src: "https://manoo089.ddns.net/site/music/Waterdrop_Final.flac",
+        src: "https://manoo089.ddns.net/music-player/music/Waterdrop_Final.flac",
     },
     {
         title: "Soulig",
         artist: "Manoo & Shirin",
         cover: "img/nocover.jpg",
-        src: "https://manoo089.ddns.net/site/music/Soulig_Neu.flac",
+        src: "https://manoo089.ddns.net/music-player/music/Soulig_Neu.flac",
     },
 ];
